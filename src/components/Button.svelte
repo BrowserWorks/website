9792f742eb6ecd66dd467a8143359266eb9ee19f
Sylvain <hello@sylvainsimao.fr>
2023-08-01 12:43:15 +1000

    Added os-specific download button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -7,36 +7,19 @@
 	export let type: 'submit' | 'reset' | 'button' | undefined = 'button'
 
 	export { className as class }
-
-	let tag: 'a' | 'button' = href ? 'a' : 'button'
-	let role: 'link' | 'button' = href ? 'link' : 'button'
-	let target: '_blank' | '_self' | undefined
-	let rel: 'prefecth' | 'noopener nofollow' | undefined
-
-	if (tag === 'a') {
-		type = undefined
-		rel = 'prefecth'
-
-		if (href && href?.startsWith('http')) {
-			target = '_blank'
-			rel = 'noopener nofollow'
-		} else if (href) {
-			target = '_self'
-		}
-	}
 </script>
 
 <svelte:element
-	this={tag}
+	this={href ? 'a' : 'button'}
 	on:click|stopPropagation
 	on:keydown
 	{href}
-	{type}
-	{role}
-	{rel}
-	{target}
+	type={!href ? type : undefined}
+	role={href ? 'link' : 'button'}
+	rel={href && href?.startsWith('http') ? 'noopener nofollow' : href ? 'prefecth' : undefined}
+	target={href && href?.startsWith('http') ? '_blank' : '_self'}
 	class={twMerge(
-		'whitespace-nowrap rounded-full bg-gold px-7 py-4 font-montserrat text-lg font-bold font-semibold no-underline transition-all hover:scale-105 hover:bg-sand lg:text-xl',
+		'whitespace-nowrap rounded-full bg-gold px-7 py-4 font-montserrat text-lg font-semibold no-underline transition-all hover:scale-105 hover:bg-sand lg:text-xl',
 		className
 	)}
 	{...$$restProps}
---
+++ src/components/DownloadButton.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import { UAParser } from 'ua-parser-js'
+	import Button from '~/components/Button.svelte'
+	import { downloadLinks } from '~/config'
+	import GithubReleases from '~/generated/releases.json'
+	import { localizeUrl, useTranslations } from '~/i18n/utils'
+
+	export let lang: string = 'en'
+
+	const t = useTranslations(lang as any)
+	const latest = GithubReleases?.find((release) => !Boolean(release?.prerelease))
+
+	let href = localizeUrl('/download', lang)
+
+	onMount(() => {
+		if (latest) {
+			const uap = UAParser(navigator.userAgent)
+			const os = uap.os.name.toLowerCase()
+			const isMacOS = ['mac os'].includes(os)
+			const isWindows = ['windows'].includes(os)
+			const isLinux = ['ubuntu', 'linux', 'unix', 'gnu', 'fedora'].includes(os)
+
+			// https://faisalman.github.io/ua-parser-js-docs/v2/api/ua-parser-js/get-os.html
+			if (isMacOS) {
+				href = downloadLinks.macOS(latest.tag_name)
+			} else if (isWindows) {
+				href = downloadLinks.windows(latest.tag_name)
+			} else if (isLinux) {
+				href = downloadLinks.linux(latest.tag_name)
+			}
+		}
+	})
+</script>
+
+<Button {href}>{t('cta.download')}</Button>
