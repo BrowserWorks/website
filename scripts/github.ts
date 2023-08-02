bd825599eebcb953b4b47915e2428f72a9e54c3e
Sylvain <hello@sylvainsimao.fr>
2023-08-02 11:25:47 +1000

    build-time hashes

--- scripts/github.ts
+++ scripts/github.ts
@@ -1,10 +1,44 @@
 import { writeFile } from 'fs/promises'
+import { sha512 } from 'hash-wasm'
 import { join } from 'path'
+import { downloadLinks } from '../src/config'
 
 async function pull() {
 	try {
 		const data = await fetch('https://api.github.com/repos/WaterfoxCo/Waterfox/releases')
-		const releases = await data.json()
+		const json = await data.json()
+		const releases: any[] = []
+
+		for (let idx = 0; idx < json.length; idx++) {
+			const release = json[idx]
+
+			const windows = downloadLinks.windows(release.tag_name)
+			const macOS = downloadLinks.macOS(release.tag_name)
+			const linux = downloadLinks.linux(release.tag_name)
+
+			release.downloads = [
+				{
+					id: 'windows',
+					label: 'Windows',
+					link: windows,
+					hash: await sha512(windows + '.sha512')
+				},
+				{
+					id: 'macos',
+					label: 'macOS',
+					link: macOS,
+					hash: await sha512(macOS + '.sha512')
+				},
+				{
+					id: 'linux',
+					label: 'Linux',
+					link: linux,
+					hash: await sha512(linux + '.sha512')
+				}
+			]
+
+			releases.push(release)
+		}
 
 		await writeFile(
 			join(__dirname, '../src/generated/releases.json'),
