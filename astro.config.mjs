cb022c72a9b33843c3bf420cd3bd6364802a3830
Alex Kontos <MrAlex94@users.noreply.github.com>
2023-10-16 11:36:15 +0100

    Add blog component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+import blog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		blog({
+			authors: {
+				alex: {
+				  name: 'Alex Kontos',
+				  title: 'Founder',
+				  picture: '/alex.jpg', // Images in the `public` directory are supported.
+				  url: 'https://uk.linkedin.com/in/alex-kontos',
+				},
+			}
+		}),
 		starlight({
+			components: {
+				MarkdownContent: 'starlight-blog/overrides/MarkdownContent.astro',
+				Sidebar: 'starlight-blog/overrides/Sidebar.astro',
+				ThemeSelect: 'starlight-blog/overrides/ThemeSelect.astro',
+			},
 			title: 'My Docs',
 			social: {
 				github: 'https://github.com/withastro/starlight',
---
+++ src/content/docs/blog/first.md
@@ -0,0 +1,9 @@
+---
+author: Alex
+date: 2023-07-24
+title: My first blog post
+---
+
+## Hello
+
+Hello world!
