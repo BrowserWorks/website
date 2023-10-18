1defbae6025d28e918f1c5395d5835942ad3f37c
Alex Kontos <MrAlex94@users.noreply.github.com>
2023-10-18 21:29:29 +0100

    Make tailwind and starlight play nice.
    Only apply preflight to elements we want.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const { scopedPreflightStyles } = require('tailwindcss-scoped-preflight')
 const starlightPlugin = require('@astrojs/starlight-tailwind')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [require('@tailwindcss/typography')],
+	corePlugins: {
+		preflight: false
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		scopedPreflightStyles({
+			cssSelector: '.tailwind-preflight',
+			mode: 'matched only'
+		}),
+		starlightPlugin()
+	],
 	theme: {
 		extend: {
 			fontFamily: {
